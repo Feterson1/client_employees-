ed38ae241ecaa903a908964a74f9d9cd12ec4865
Feterson <feterson@bk.ru>
2024-04-09 01:27:11 +0500

    theme-provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,33 @@ import App from "./App"
 import { store } from "./app/store"
 import "./index.css"
 import { NextUIProvider } from "@nextui-org/react"
+import { createBrowserRouter } from "react-router-dom"
+import { ThemeProvider } from "./components/theme-provider"
 
 const container = document.getElementById("root")
+const router = createBrowserRouter([
+  {
+    path: "/auth",
+    element: <h1>Auth</h1>,
+  },
+  {
+    path: "/",
+    element: <h1>layout</h1>,
+  },
+])
 
 if (container) {
   const root = createRoot(container)
 
   root.render(
     <React.StrictMode>
-      <NextUIProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </NextUIProvider>
+      <Provider store={store}>
+        <NextUIProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </NextUIProvider>
+      </Provider>
     </React.StrictMode>,
   )
 } else {
