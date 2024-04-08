7d60c26def857ef5cb716d67558b937cc330eaf9
Feterson <feterson@bk.ru>
2024-04-09 00:12:42 +0500

    userApi

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/react"
+const App = () => {
+  return (
+    <div className="App">
+      <Button color="primary">Нажми</Button>
+    </div>
+  )
+}
+
+export default App
